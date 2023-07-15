3ee78371c52b07b7824ce86a0f0423342c1d3aca
assadsid <138975909+assadsid@users.noreply.github.com>
2023-07-15 12:26:01 +0500

    Add files via upload

---
+++ exe01.js
@@ -0,0 +1,23 @@
+// This exercise is assign a specific data type to variable
+// let fn:string = "Assad"
+// let ln:string = "Siddiqui"
+// fn = "Kamil"
+// console.log(`${fn} ${ln}`);
+// This exercise is apply minus arithmetic operator
+// let price = 1800;
+// let disc = 275;
+// let total = price-disc;
+// console.log(total);
+// This exercise is apply multiply assignment operator
+// let price = 25;
+// let tax = 7;
+// let final = (price % tax);
+// console.log(final);
+// This exercise is apply template literals in console
+// let nm1 = "John";
+// let message = 'would you like to learn some "Python" today?'
+// console.log(`Hello ${nm1},\n${message}`);
+var firstName = "Assad";
+var lastName = "Siddiqui";
+var fullName = (firstName + " " + lastName);
+console.log(fullName);
---
+++ exe01.ts
@@ -0,0 +1,29 @@
+// This exercise is assign a specific data type to variable
+// let fn:string = "Assad"
+// let ln:string = "Siddiqui"
+// fn = "Kamil"
+// console.log(`${fn} ${ln}`);
+
+// This exercise is apply minus arithmetic operator
+// let price = 1800;
+// let disc = 275;
+// let total = price-disc;
+// console.log(total);
+
+// This exercise is apply multiply assignment operator
+// let price = 25;
+// let tax = 7;
+// let final = (price % tax);
+// console.log(final);
+
+// This exercise is apply template literals in console
+// let nm1 = "John";
+// let message = 'would you like to learn some "Python" today?'
+// console.log(`Hello ${nm1},\n${message}`);
+
+// This exercise is apply concatenate between two variables to join them
+// let firstName = "Assad";
+// let lastName = "Siddiqui";
+// let fullName = (firstName + " " + lastName);
+// console.log(fullName);
+
